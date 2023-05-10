3ea61c10e9a06693eb4f2ecf54f4f58082bb2c24
Eric Gach <eric.gach@gmail.com>
2023-05-09 22:54:36 -0500

    Updating navigation toggle

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import { Entry } from "contentful";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "@/assets/logo.png";
 import styles from "@/styles/layout.module.scss";
@@ -26,7 +26,14 @@ export default function Layout({ props, children }: LayoutProps) {
 	const { page, pages, siteSettings, socialIcons } = props;
 	const externalPages = pages?.filter(page => page.fields.slug !== "home");
 	const navItems = {sections: page?.fields.content.filter(section => section.sys.contentType.sys.id === "pageSection"), pages: externalPages} as { sections: IPageSection[], pages: IPage[]};
+	const [navOpen, setNavOpen] = useState(false);
 	const siteTitle = (siteSettings["site.title"] || "My Portfolio") + (page.fields.title? ` | ${page.fields.title}` : '');
+
+	function handleClick() {
+		document.body.classList.toggle(styles.showNav);
+		document.getElementsByTagName("html")[0].style.overflowY = !navOpen? "hidden" : "scroll";
+		setNavOpen((prev) => !prev);
+	}
 
 	return <>
 		<Head>
@@ -45,11 +52,12 @@ export default function Layout({ props, children }: LayoutProps) {
 		<footer className={styles.footer}>
 			<div>
 				<section><Icon icon="FaRegCopyright" /> Eric Gach All Rights Reserved.</section>
-				<Icon icon="FaEllipsisV" />
+				<Icon className={styles.icon} icon="FaEllipsisV" />
 				<section>
 					<a href="https://github.com/BigE/portfolio" target="_blank">Portfolio<Icon icon="FaExternalLinkAlt" link /></a>	is built using the <a href="https://nextjs.org" target="_blank">NextJS<Icon icon="FaExternalLinkAlt" link /></a> framework and <a href="https://contentful.com" target="_blank">Contentful CMS<Icon icon="FaExternalLinkAlt" link /></a>
 				</section>
 			</div>
 		</footer>
+        <a id="menuToggle" className={styles.menuToggle} onClick={handleClick}><Icon className={styles.toggle} icon="FaBars" /></a>
 	</>
 }
