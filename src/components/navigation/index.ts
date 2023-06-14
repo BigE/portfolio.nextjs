0b7650edb365041737e7e440095a49e3ede4196c
Eric Gach <eric.gach@gmail.com>
2023-06-14 13:27:19 -0500

    Updating resume

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,3 +1,4 @@
 export { default as Navigation } from "./primary";
 export type { NavigationProps } from "./primary";
 export { default as SocialNavigation } from "./social";
+export { default as MenuToggle } from "./toggle";
--- src/components/resume/index.tsx
+++ src/components/resume/index.tsx
@@ -1,44 +1,58 @@
+import { Document } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import { TypeResume } from "@/@types/contentful/TypeResume";
-
 import styles from "@/styles/resume.module.scss";
+import { renderPersonalExperience, renderProfessionalExperience } from "./experience";
 
 export type ResumeProps = {
-	resume: TypeResume<"WITHOUT_UNRESOLVABLE_LINKS", string>;
+	objective: string;
+	skills: Document;
+	professionalExperience?: React.ReactNode;
+	personalExperience?: React.ReactNode;
 };
 
-export default function Resume(props: ResumeProps) {
-	return <div className={styles.resume}>
+export default function Resume({
+	objective,
+	skills,
+	professionalExperience,
+	personalExperience,
+	...props
+}: ResumeProps & JSX.IntrinsicElements["div"]) {
+	return <div className={styles.resume} {...props}>
 		<section id="objective" className={`${styles.section} ${styles.objective}`}>
 			<h2>Objective</h2>
-			<div className={styles.content}>{props.resume.fields.objective}</div>
+			<div className={styles.content}>{objective}</div>
 		</section>
 		<section id="skills" className={`${styles.section} ${styles.skills}`}>
 			<h2>Skills</h2>
-			<div className={styles.content}>{documentToReactComponents(props.resume.fields.skills)}</div>
+			<div className={styles.content}>{documentToReactComponents(skills)}</div>
 		</section>
-		<section className={styles.section}>
-			<h2>Professional Experience</h2>
-			{props.resume.fields.professionalExperience?.map(experience => {
-				if (!experience) return <></>;
-
-				console.log(experience);
-
-				return <section key={experience.sys.id} className={styles.content}>
-					<header>
-						<h3>{experience.fields.companyName}</h3>
-						<h4>{experience.fields.positionHeld.join(', ')}</h4>
-						<h5>{experience.fields.startDate}{experience.fields.endDate? ` to ${experience.fields.endDate}` : ''}</h5>
-					</header>
-				</section>
-			})}
-		</section>
+		{professionalExperience && 
+			<section className={styles.section}>
+				<h2>Professional Experience</h2>
+				<section className={styles.content}>{professionalExperience}</section>
+			</section>
+		}
+		{personalExperience && 
+			<section className={styles.section}>
+				<h2>Personal Experience</h2>
+				<section className={styles.content}>{personalExperience}</section>
+			</section>
+		}
 	</div>
 }
 
 export function renderResume(resume: TypeResume<"WITHOUT_UNRESOLVABLE_LINKS", string>) {
 	if (!resume) return <></>;
 
-	return <Resume key={resume.sys.id} resume={resume}></Resume>
+	return <Resume
+		key={resume.sys.id}
+		objective={resume.fields.objective}
+		skills={resume.fields.skills}
+		professionalExperience={resume.fields.professionalExperience?.map(experience => renderProfessionalExperience(experience))}
+		personalExperience={resume.fields.personalExperience?.map(experience => renderPersonalExperience(experience))}
+	/>
 }
+
+export { default as Experience, renderPersonalExperience, renderProfessionalExperience } from './experience';
