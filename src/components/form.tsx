ed7be27960fcfd4d720e3418dc05ec013bb949ae
Eric Gach <eric.gach@gmail.com>
2024-02-23 15:50:49 -0600

    Updating form to enable submissions
    This update also affects the button, since how I was passing the arguments was
    actually incorrect.

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,5 +4,5 @@ import { TypeForm } from "@/@types/contentful";
 import Form from "./client/form";
 
 export default async function renderForm(form: TypeForm<"WITHOUT_UNRESOLVABLE_LINKS", string>) {
-	return <Form key={form.sys.id} id={form.sys.id}></Form>;
+	return <Form key={form.sys.id} id={form.sys.id} endpoint={form.fields.url}></Form>;
 }
